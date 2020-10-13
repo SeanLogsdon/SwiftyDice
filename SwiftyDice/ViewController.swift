4897f6ae7a2051a84fc14105e93edbeb7873e404
Sean Logsdon <seanlogsdon14@gmail.com>
2020-10-13 08:32:15 -0500

    iOS dice hehe

---
+++ SwiftyDice/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwiftyDice
+//
+//  Created by Sean Logsdon on 10/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // we need to do cool stuff here hehe
+    }
+
+
+}
+
