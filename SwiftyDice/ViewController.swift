0b91e6142d5062896556f594b84cfbbd6f88ad93
Sean Logsdon <seanlogsdon14@gmail.com>
2020-10-19 22:30:42 -0500

    for sound to play or not to play

--- SwiftyDice/ViewController.swift
+++ SwiftyDice/ViewController.swift
@@ -8,6 +8,8 @@ import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
+    
+
 
     @IBOutlet weak var diceImageView: UIImageView!
     @IBOutlet var criticalLabel: UILabel!
@@ -22,8 +24,77 @@ class ViewController: UIViewController {
         rollDice()
     }
     
+    var player: AVAudioPlayer?
+    var player2: AVAudioPlayer?
+    var player3: AVAudioPlayer?
+    
+   // func yTown { print "WWDC Swift is here" }
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "rolldice", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+            print("it work")
+
+
+        } catch {
+            print("it dont work")
+        }
+    }
+    
+    func playSound2() {
+        guard let url = Bundle.main.url(forResource: "fanfare", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+
+            player2 = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+
+            guard let player2 = player2 else { return }
+
+            player2.play()
+            print("it work2")
+
+
+        } catch {
+            print("it dont work2")
+        }
+    }
+    
+    func playSound3() {
+        guard let url = Bundle.main.url(forResource: "failure", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+
+            player3 = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player3 = player3 else { return }
+
+            player3.play()
+            print("it work3")
+
+
+        } catch {
+            print("it dont work3")
+        }
+    }
+    
     func rollDice() {
         // do cool stuff
+        
         print("we rollin but in a car")
         
         let rolledNumber = Int.random(in: 1...20)
@@ -34,10 +105,13 @@ class ViewController: UIViewController {
         
         if (imageName == "d1") {
             criticalLabel.text = "Noob"
+            playSound3()
         } else if (imageName == "d20") {
             criticalLabel.text = "Epic Bruh Moment"
+            playSound2()
         } else {
             criticalLabel.text = ""
+            playSound()
         }
     }
     
@@ -45,6 +119,8 @@ class ViewController: UIViewController {
         rollDice()
     }
     
-   // func yTown { print "WWDC Swift is here" }
+
+    
+    
 }
 
